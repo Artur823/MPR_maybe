ffd3e39c2a843370c1b41ede7de9dd34e07b8aba
Artur823 <s31646@pjwstk.edu.pl>
2024-12-22 21:55:24 +0100

    Połączyłem frontend z backendem i zrobiłem /view/all

--- MPR_spring/src/main/java/pl/edu/pjatk/MPR_spring/exception/CapybaraNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pjatk.MPR_spring.exception;
-
-public class CapybaraNotFoundException extends RuntimeException {
-    public CapybaraNotFoundException() {
-        super("Capybara not found ");
-    }
-}
